616563ccb2dee8c191d2b81c2a9e0dad0a64fe08
lvmphung <minhphung435@gmail.com>
2019-10-01 11:08:51 +0700

    no message

---
+++ AugenProject.Web.Common/IClientRequestHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace AugenProject.Web.Common
+{
+    public interface IClientRequestHelper
+    {
+        string SendRequest(WebHeaderCollection headerCollection, string address, string method, string data = "");
+
+    }
+}
