3d30d7918a0c3ed95cefafea520e1bcb62bcf511
lvmphung <minhphung435@gmail.com>
2019-10-01 11:02:01 +0700

    no message

---
+++ AugenProject.Common/Serialization/Serialization.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace AugenProject.Common.Serialization
+{
+    public class SerializationHelper : ISerialization
+    {
+        public T DeserializeJsonToObject<T>(string jsonString)
+        {
+            if (string.IsNullOrEmpty(jsonString))
+                throw new ArgumentNullException("Can not deserialize " + jsonString);
+
+            return JsonConvert.DeserializeObject<T>(jsonString);
+        }
+
+        public T ToObject<T>(object obj)
+        {
+            return JObject.FromObject(obj).ToObject<T>();
+        }
+    }
+}
