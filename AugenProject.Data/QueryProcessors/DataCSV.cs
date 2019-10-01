1feb4509cfdab453c4f0db30427549b44e743a1f
lvmphung <minhphung435@gmail.com>
2019-10-01 14:26:47 +0700

    no message

--- AugenProject.Data/QueryProcessors/DataCSV.cs
+++ AugenProject.Data/QueryProcessors/DataCSV.cs
@@ -27,20 +27,43 @@ namespace AugenProject.Data.QueryProcessors
                         if (!string.Equals(headerLine, line))
                         {
                             var values = line.Split(',');
-                            customers.Add(new CustomerEntity()
+                            if (values[2].Contains("\""))
                             {
-                                FirstName = values[0],
-                                LastName = values[1],
-                                CompanyName = values[2],
-                                Address = values[3],
-                                City = values[4],
-                                State = values[5],
-                                Post = values[6],
-                                Phone1 = values[7],
-                                Phone2 = values[8],
-                                Email = values[9],
-                                Web = values[10]
-                            });
+                                var company = values[2] + values[3];
+                                company = company.Replace("\"", "");
+                                customers.Add(new CustomerEntity()
+                                {
+                                    FirstName = values[0],
+                                    LastName = values[1],
+                                    CompanyName = company,
+                                    Address = values[4],
+                                    City = values[5],
+                                    State = values[6],
+                                    Post = values[7],
+                                    Phone1 = values[8],
+                                    Phone2 = values[9],
+                                    Email = values[10],
+                                    Web = values[11]
+                                });
+                            }
+                            else
+                            {
+                                customers.Add(new CustomerEntity()
+                                {
+                                    FirstName = values[0],
+                                    LastName = values[1],
+                                    CompanyName = values[2],
+                                    Address = values[3],
+                                    City = values[4],
+                                    State = values[5],
+                                    Post = values[6],
+                                    Phone1 = values[7],
+                                    Phone2 = values[8],
+                                    Email = values[9],
+                                    Web = values[10]
+                                });
+                            }
+                            
                         }
                     }
                 }
